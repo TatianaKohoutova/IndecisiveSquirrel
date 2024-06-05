002a9abf2a50d6f86abd2be2fc438e72c0c10487
Barbora Tesařová <tesarovabara@seznam.cz>
2024-06-05 11:42:32 +0200

    Připojení Main na HomePage.

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,7 @@
 import { Header } from '../Components/Header';
 // import { AboutUs } from './AboutUs/about';
 // import { AboutGame } from './AboutGame/game';
+import { Main } from '../Components/Main';
 import { Footer } from '../Components/Footer';
 
 import '/global.css';
@@ -8,6 +9,7 @@ export const HomePage = () => {
   return (
     <>
       <Header />
+      <Main/>
       {/* <AboutUs />
       <AboutGame /> */}
       <Footer />

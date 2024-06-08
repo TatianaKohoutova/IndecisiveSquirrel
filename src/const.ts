07a6522c2e9bb299d296be44c5cb4a962572b79c
Barbora Tesařová <tesarovabara@seznam.cz>
2024-06-08 10:46:53 +0200

    Rewriting recipes.js to TS and creating a map for the units.

---
+++ src/const.ts
@@ -0,0 +1,23 @@
+export enum Units {
+  Kg = 'Kg',
+  L = 'L',
+  Ml = 'Ml',
+  G = 'G',
+  TableSpoon = 'TableSpoon',
+  TeaSpoon = 'TeaSpoon',
+  Piece = 'Piece',
+  Handful = 'Handful',
+  Pinch = 'Pinch'
+}
+
+export const UnitsMap = {
+  [Units.Kg]: 'Kg',
+  [Units.L]: 'Litr',
+  [Units.Ml]: 'Mililitr',
+  [Units.G]: 'Gramů',
+  [Units.TableSpoon]: 'Lžíce',
+  [Units.TeaSpoon]: 'Čajova lžička',
+  [Units.Piece]: 'Ks',
+  [Units.Handful]: 'Hrst',
+  [Units.Pinch]: 'Pinch'
+}

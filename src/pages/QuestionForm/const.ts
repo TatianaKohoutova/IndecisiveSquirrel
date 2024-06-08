e5b76b40e0cbd0369da4156d1e26c0927657be4a
Barbora Tesařová <tesarovabara@seznam.cz>
2024-06-08 15:41:41 +0200

    More questions for form.

--- src/pages/QuestionForm/const.ts
+++ src/pages/QuestionForm/const.ts
@@ -44,12 +44,45 @@ export const questions = {
     },
     questionType: {
       id: 'questionType',
-      title: 'Jak  typ jídla by ti pro dnešek vyhovoval?',
+      title: 'Jak typ jídla by ti pro dnešek vyhovoval?',
       options: [
           { name: 'Veganské', img: '', value: '' },
-          { name: 'Vegeta', img: '', value: '' },
-          { name: 'Středně pálivé.', img: '', value: '' },
-          { name: 'Miluju pálivé.', img: '', value: '' },
+          { name: 'Vegetariánské', img: '', value: '' },
+          { name: 'Masové', img: '', value: '' }
       ],
   },
+  questionActivity: {
+    id: 'questionActivity',
+    title: 'Jak jsi byl dneska aktivní?',
+    options: [
+        { name: 'Aktivní', img: '', value: '' },
+        { name: 'Středně', img: '', value: '' },
+        { name: 'Pasivní', img: '', value: '' }
+    ],
+},
+questionDifficulty: {
+  id: 'questionDifficulty',
+  title: 'Jakou náročnost receptu by jsi dneska preferoval?',
+  options: [
+      { name: 'Lehkou', img: '', value: '' },
+      { name: 'Střední', img: '', value: '' },
+      { name: 'Nebráním se něčemu náročnějšímu', img: '', value: '' }
+  ],
+},
+questionTime: {
+  id: 'questionTime',
+  title: 'Kolik máš času na vaření',
+  options: [
+      { name: 'Nemám moc času', img: '', value: '' },
+      { name: 'Jsem flexibilní', img: '', value: '' }
+  ],
+},
+questionMoney: {
+  id: 'questionMoney',
+  title: 'Chceš utrácet?',
+  options: [
+      { name: 'Chci něco úsporného', img: '', value: '' },
+      { name: 'Klidně si nějaké ingredience dokoupím', img: '', value: '' }
+  ],
+},
 }

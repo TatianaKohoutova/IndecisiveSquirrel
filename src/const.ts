3b3ce970b282e88cea47f52b195c0ea917129adb
Barbora Tesařová <tesarovabara@seznam.cz>
2024-06-08 13:22:33 +0200

    Adding values and maps for questions.

--- src/const.ts
+++ src/const.ts
@@ -1,23 +1,111 @@
 export enum Units {
-  Kg = 'Kg',
-  L = 'L',
-  Ml = 'Ml',
-  G = 'G',
-  TableSpoon = 'TableSpoon',
-  TeaSpoon = 'TeaSpoon',
-  Piece = 'Piece',
-  Handful = 'Handful',
-  Pinch = 'Pinch'
+    Kg = 'Kg',
+    L = 'L',
+    Ml = 'Ml',
+    G = 'G',
+    TableSpoon = 'TableSpoon',
+    TeaSpoon = 'TeaSpoon',
+    Piece = 'Piece',
+    Handful = 'Handful',
+    Pinch = 'Pinch',
 }
 
 export const UnitsMap = {
-  [Units.Kg]: 'Kg',
-  [Units.L]: 'Litr',
-  [Units.Ml]: 'Mililitr',
-  [Units.G]: 'Gramů',
-  [Units.TableSpoon]: 'Lžíce',
-  [Units.TeaSpoon]: 'Čajova lžička',
-  [Units.Piece]: 'Ks',
-  [Units.Handful]: 'Hrst',
-  [Units.Pinch]: 'Pinch'
+    [Units.Kg]: 'Kg',
+    [Units.L]: 'Litr',
+    [Units.Ml]: 'Mililitr',
+    [Units.G]: 'Gramů',
+    [Units.TableSpoon]: 'Lžíce',
+    [Units.TeaSpoon]: 'Čajova lžička',
+    [Units.Piece]: 'Ks',
+    [Units.Handful]: 'Hrst',
+    [Units.Pinch]: 'Pinch',
+}
+
+export enum Calories {
+    Low = 'Low',
+    Medium = 'Medium',
+    High = 'High',
+}
+
+export const CaloriesScale = {
+    [Calories.Low]: {
+        max: 299,
+    },
+    [Calories.Medium]: {
+        min: 300,
+        max: 599,
+    },
+    [Calories.High]: {
+        min: 600,
+    },
+}
+
+export enum Taste {
+    Sweet = 'Sweet',
+    Salty = 'Salty',
+    Sour = 'Sour',
+}
+export const TasteMap = {
+    [Taste.Sweet]: 'Sladké',
+    [Taste.Salty]: 'Slané',
+    [Taste.Sour]: 'Kyselé',
+}
+
+export enum Spicy {
+    NonSpicy = 'Non-spice',
+    Low = 'Low',
+    Medium = 'Medium',
+    High = 'High',
+}
+
+export const SpicyScale = {
+    [Spicy.NonSpicy]: 'Nepálivé',
+    [Spicy.Low]: 'Lehce pálivé',
+    [Spicy.Medium]: 'Středně pálivé',
+    [Spicy.High]: 'Pikantní',
+}
+
+export enum Type {
+    Vegan = 'Vegan',
+    Vegetarian = 'Vege',
+    Meat = 'Meat',
+}
+
+export const TypeMap = {
+    [Type.Vegan]: 'Veganské',
+    [Type.Vegetarian]: 'Vegetariánské',
+    [Type.Meat]: 'Masové',
+}
+
+export const ActivityType = {
+    Passive: 'Passive',
+    Medium: 'Medium',
+    Active: 'Activ',
+}
+
+// ActivityTypeTreshold as Calories number
+export const ActivityTypeTreshold = {
+    [ActivityType.Passive]: {
+        max: 299,
+    },
+    [ActivityType.Medium]: {
+        min: 300,
+        max: 599,
+    },
+    [ActivityType.Active]: {
+        min: 600,
+    },
+}
+
+export const Difficulty = {
+    Easy: 'Easy',
+    Medium: 'Medium',
+    Hard: 'Hard',
+}
+
+export const DifficultyMap = {
+    [Difficulty.Easy]: 'Snadné',
+    [Difficulty.Medium]: 'Středně náročné',
+    [Difficulty.Hard]: 'Náročné',
 }

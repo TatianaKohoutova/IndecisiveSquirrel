65f44df209fa507b29f5ddcae1f43b5699210dd0
Barbora Tesařová <tesarovabara@seznam.cz>
2024-06-09 11:39:25 +0200

    Rename buttons.

--- src/pages/Components/Menu/menu.jsx
+++ src/pages/Components/Menu/menu.jsx
@@ -19,8 +19,9 @@ export const Header = () => {
             <Link to="./" className="logo"></Link>
             <div className={`menu ${menuOpen && 'menu_open'}`}>
                 <MenuItem link={'./'} text={'Domů'} handleClick={handleClickItem} />
-                <MenuItem link={'/game'} text={'O hře'} handleClick={handleClickItem} />
+                <MenuItem link={'/game'} text={'Hrát'} handleClick={handleClickItem} />
                 <MenuItem link={'./about'} text={'O nás'} handleClick={handleClickItem} />
+                <MenuItem link={'./recepyList'} text={'Recepty'} handleClick={handleClickItem} />
             </div>
             <div className="hamburger-menu" onClick={handleClickHamb}>
                 <div className={`line ${menuOpen && 'line_open'}`}></div>

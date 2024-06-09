75860daabb5e5cb801d969c67b9f7c8cbdc346e8
Jana Jarošová <Janamjaros@seznam.cz>
2024-06-09 19:42:54 +0200

    error page adjusted

--- src/pages/Components/ErrorPage/index.jsx
+++ src/pages/Components/ErrorPage/index.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './style.css'
 
 export const ErrorPage = () => {
-  return (
-    <div>
-      <h1>Stránka nebyla nalezena</h1>
-      <p>Je nám líto, ale stránka, kterou hledáte, neexistuje.</p>
-      <Link to="/">Zpět na hlavní stránku</Link>
-    </div>
-  );
-};
+    return (
+        <div className="error-page">
+            <h1>Stránka nebyla nalezena</h1>
+            <p>Je nám líto, ale stránka, kterou hledáte, neexistuje.</p>
+            <Link to="/" className="button">
+                Zpět na hlavní stránku
+            </Link>
+            <div>
+                <img src="../img/SquirrelOnTree.png" alt="Lezoucí veverka" className="img_error" />
+            </div>
+        </div>
+    )
+}
 
-export default ErrorPage;
+export default ErrorPage

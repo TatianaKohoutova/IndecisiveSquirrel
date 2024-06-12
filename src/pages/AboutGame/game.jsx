187e7f9c0e6b4563684e59ada11fe619acf326f0
Barbora Tesařová <tesarovabara@seznam.cz>
2024-06-12 09:50:07 +0200

    Renaming to the correct language, deleting unused parts of the code, deleting return and adding round brackets.

--- src/pages/AboutGame/game.jsx
+++ src/pages/AboutGame/game.jsx
@@ -1,15 +1,14 @@
 import { Link } from 'react-router-dom'
 import './game.css'
 
-export const AboutGame = () => {
-    return (
+export const AboutGame = () => (
         <div>
-            <div className="uvod">
+            <div className="introduction">
                 <h1>Nerozhodná veverka</h1>
             </div>
             <div className="text bubble">
                 <div className="main_content">
-                    <img className="bubbleArrow" src="../img/Arrow.png" alt="Bubble arrow" />
+                    <img className="bubbleArrow" src="../img/Arrow.png" alt="Šipka k bublině" />
                     <h3>O hře</h3>
                     <p className="bubble">
                         Pravidla hry jsou jednoduchá. Mám pro tebe pár jednoduchých otázek, na základě kterých ti
@@ -25,8 +24,6 @@ export const AboutGame = () => {
                 </div>
             </div>
             <img src="../img/Volba.png" alt="Volba" className="img_cornerSquirrel--bigscreen" />
-
-            <div className="footer bottom-line"></div>
         </div>
     )
-}
+

b14bdea77c8eb273772386962a1b2029b286c5df
Barbora Tesařová <tesarovabara@seznam.cz>
2024-06-13 09:54:11 +0200

    Reinstall ESLint.

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,16 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
-import pluginReact from 'eslint-plugin-react'
-import { fixupConfigRules } from '@eslint/compat'
 
 export default [
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     { files: ['**/*.jsx'], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-    ...fixupConfigRules(pluginReactConfig),
-    rules,
+    pluginReactConfig,
     {
-        'react/react-in-jsx-scope': 'off',
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+        },
     },
 ]
